3d9a29e3f4494b8d30d0deaabb8d3a5a49cd09ff
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-01-25 14:25:15 -0500

    added (almost everything for) models

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {    
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // getter method to format timestamp on query?
+        },
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', userSchema);
+
+module.exports = Thought;
